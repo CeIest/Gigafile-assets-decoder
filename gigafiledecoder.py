19a0f5146b45b03565017b2474aad0d474819260
CeIest <45696416+CeIest@users.noreply.github.com>
2023-01-25 09:11:43 +0100

    Update gigafiledecoder.py

--- gigafiledecoder.py
+++ gigafiledecoder.py
@@ -1,18 +1,22 @@
 import os
 from tkinter import Tk
 from tkinter.filedialog import askdirectory
+import shutil
 
 Tk().withdraw()
 folder_path = askdirectory()
+destination_folder = folder_path + "_decrypted"
 
-for root, _, files in os.walk(folder_path):
+for root, dirs, files in os.walk(folder_path):
+    dest_dir = os.path.join(destination_folder, os.path.relpath(root, folder_path))
+    os.makedirs(dest_dir, exist_ok=True)
+
     for file_name in files:
         original_filepath = os.path.join(root, file_name)
         decoded_filename = file_name.encode("cp850", "ignore").decode("shiftjis")
         print("decrypting " + decoded_filename)
-        decoded_filepath = os.path.join(root, decoded_filename)
-        os.rename(original_filepath, decoded_filepath)
+        decoded_filepath = os.path.join(dest_dir, decoded_filename)
+        shutil.copy2(original_filepath, decoded_filepath)
         
 # To-do:
 # • Decode the folder names
-# • Copy the decoded folder while keeping the original folder intact
