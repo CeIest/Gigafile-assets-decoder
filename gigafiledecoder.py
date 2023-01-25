ddfbf6867f538c065c6ce4f8962ce46621dcc7ee
CeIest <45696416+CeIest@users.noreply.github.com>
2023-01-25 12:27:25 +0100

    Update gigafiledecoder.py

--- gigafiledecoder.py
+++ gigafiledecoder.py
@@ -8,7 +8,7 @@ Tk().withdraw()
 folder_path = askdirectory()
 destination_folder = folder_path + "_decrypted"
 
-print('\ndecrypting folder "' + os.path.basename(folder_path) + '"...\n')
+print('\nDecoding folder "' + os.path.basename(folder_path) + '"...\n')
 
 for root, dirs, files in os.walk(folder_path):
     dest_dir = os.path.join(destination_folder, os.path.relpath(root, folder_path))
@@ -17,7 +17,7 @@ for root, dirs, files in os.walk(folder_path):
     for file_name in files:
         original_filepath = os.path.join(root, file_name)
         decoded_filename = file_name.encode("cp850", "ignore").decode("shiftjis")
-        print("decrypting file  " + decoded_filename)
+        print("Decoding file  " + decoded_filename)
         decoded_filepath = os.path.join(dest_dir, decoded_filename)
         shutil.copy2(original_filepath, decoded_filepath)
 print('\n')
@@ -29,7 +29,7 @@ print('\n')
 #                 dir_name = dir_name.rstrip('ò')
 #             original_filepath = os.path.join(root, dir_name)
 #             decoded_dirname = dir_name.encode("cp850", "ignore").decode("shiftjis")
-#             print("decrypting folder  " + decoded_dirname)
+#             print("Decoding folder  " + decoded_dirname)
 #             decoded_filepath = os.path.join(root, decoded_dirname)
 #             os.rename(original_filepath, decoded_filepath)
 #             rename_subdirectories(decoded_filepath)
